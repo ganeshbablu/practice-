07eb90653075bbf14c9e042d0450f9f9a44e3c6a
Ganesh <110025487+ganeshbablu@users.noreply.github.com>
2022-08-05 18:00:46 +0530

    Add files via upload

---
+++ program 4/Fun1.java
@@ -0,0 +1,23 @@
+class Fun1{
+    static int i=0;
+	
+	public static void main(String[]args)
+	{
+	
+	    System.out.println("From Main Begin");
+		Address();
+		Address();
+		Address();
+		Address();
+		Address();
+		Address();
+		Address();
+		Address();
+		
+	}
+    static void Address()
+    {
+        System.out.println("From Method 1 = "+ i);
+    }		
+
+}
